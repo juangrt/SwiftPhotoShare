f98881b2442dcaec24c6c43321a02cd4c947e2bb
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-22 15:25:44 -0400

    * Connected Image Picker to Button Press Event * Uploads Image if Image is Picked

--- SwiftImageUploader/ViewController.swift
+++ SwiftImageUploader/ViewController.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2016 ZongWare. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var uploader: Uploader!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func selectImageBtnPressed(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+            
+            Uploader.uploadFile(pickedImage)
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
+
