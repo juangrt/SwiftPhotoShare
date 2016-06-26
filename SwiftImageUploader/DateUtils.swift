46b9f6ad55e603b0d56589d4c700cdb826b10968
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-26 19:30:22 -0400

    DateUtils ISO Date String to NSDate

--- SwiftImageUploader/DateUtils.swift
+++ SwiftImageUploader/DateUtils.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class DateUtils: AnyObject {
+    
+    static func stringToDate(date:String) -> NSDate {
+        let formatter = NSDateFormatter()
+        
+        // Format 1
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        if let parsedDate = formatter.dateFromString(date) {
+            return parsedDate
+        }
+        
+        // Format 2
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:SSSZ"
+        if let parsedDate = formatter.dateFromString(date) {
+            return parsedDate
+        }
+        
+        // Couldn't parsed with any format. Just get the date
+        let splitedDate = date.componentsSeparatedByString("T")
+        if splitedDate.count > 0 {
+            formatter.dateFormat = "yyyy-MM-dd"
+            if let parsedDate = formatter.dateFromString(splitedDate[0]) {
+                return parsedDate
+            }
+        }
+        
+        // Nothing worked! Throw Error?
+        return NSDate()
+    }
+}
