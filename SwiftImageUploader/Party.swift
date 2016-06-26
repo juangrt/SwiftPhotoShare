7605d20204cabba832cf1020bb5a14dbaca1bb16
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-26 19:30:47 -0400

    Party Model Implementation and Getter

--- SwiftImageUploader/Party.swift
+++ SwiftImageUploader/Party.swift
@@ -7,12 +7,35 @@
 //
 
 import Foundation
+import UIKit
 
 class Party: AnyObject {
     
-    static func getParties(page:NSNumber) -> [Party] {
-        
-        return []
+    var title:String = ""
+    var slug:String = ""
+    var date:NSDate = NSDate()
+    
+    init(title:String , slug:String, date:NSDate){
+        self.title = title
+        self.slug = slug
+        self.date = date
+    }
+    
+    static func getParties(page:NSNumber, completion: (result: [Party]) -> Void) {
+        PhotoShareService.sharedInstance.get(.PARTY, page: page, completion: {result in
+            var parties = [Party]()
+            
+            for partyData in (result as? NSArray)! {
+                let theDate:NSDate = DateUtils.stringToDate(partyData["date"] as! String)
+                let title:String = partyData["title"] as! String
+                let slug:String = partyData["slug"] as! String
+                
+                let party = Party(title: title, slug: slug, date: theDate)
+                
+                parties.append(party)
+            }
+            completion(result: parties)
+        })
     }
     
     
