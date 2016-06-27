b92fcddef42abad5aba4f276e4966702fcfd04b8
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-27 09:46:11 -0400

    PartyCollectionCell Init

---
+++ SwiftImageUploader/PartyCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PartyCell.swift
+//  SwiftImageUploader
+//
+//  Created by Juan Carlos Garzon on 6/27/16.
+//  Copyright © 2016 ZongWare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PartyCell: UICollectionViewCell {
+    
+    
+    
+    
+}
