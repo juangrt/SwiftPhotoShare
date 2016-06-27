e7d52b1cabb77112774fcf3e5cbad1ffb69ccc0f
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-27 13:45:53 -0400

    Added placeholder image for party collection cells

---
+++ SwiftImageUploader/UIImageViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageViewExtension.swift
+//  SwiftImageUploader
+//
+//  Created by Juan Carlos Garzon on 6/27/16.
+//  Copyright © 2016 ZongWare. All rights reserved.
+//
+
+import Foundation
