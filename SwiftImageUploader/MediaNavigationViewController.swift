d53d346c4b8a566cf628174d2b86c3cec4f77e0a
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-07-04 09:04:37 -0400

    Added MediaNavigationViewController to store the current Party that the Media belongs too.

---
+++ SwiftImageUploader/MediaNavigationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MediaNavigationViewController.swift
+//  SwiftImageUploader
+//
+//  Created by Juan Carlos Garzon on 7/4/16.
+//  Copyright © 2016 ZongWare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MediaNavigationViewController:UINavigationController {
+    
+    var currentParty:Party!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
